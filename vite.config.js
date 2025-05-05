c369327bd69351a9af2407048de83b09af90fb88
MendesDesktop <jorgefmendes@hotmail.com>
2025-05-05 09:35:37 +0100

    Fix error in Lint

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   preview: {
     host: true, // Permite conexões externas
-    port: process.env.PORT || 10000, // Usa a porta definida pelo Render
     allowedHosts: ["https://news-explorer-front-final-project.onrender.com"], // Permite esse domínio
   }
 });
